899358ead6e78dd3534004203ab4e9a0663e9074
Blockbuster1234 <93834143+Blockbuster1234@users.noreply.github.com>
2026-05-18 18:46:32 +0200

    Auto: Ebola post

--- breakingnewsboulevard/dist/js/posts.js
+++ breakingnewsboulevard/dist/js/posts.js
@@ -9,3 +9,12 @@ const posts = [
     image: '/posts/test-1779118698-hero.jpg'
 }
 ];
+{
+    slug: 'at-least-100-deaths-reported-in-ebola-outbreak-in-dr-congo-as-six-americans-exposed',
+    title: '```json',
+    excerpt: '{   "title": "At Least 100 Deaths Reported in Ebola Outbreak in DR Congo as Six Americans Exposed",   "body": "<p>At least 100 people have died in the',
+    date: '2026-05-18',
+    category: 'World',
+    url: '/posts/at-least-100-deaths-reported-in-ebola-outbreak-in-dr-congo-as-six-americans-exposed.html',
+    image: '/posts/at-least-100-deaths-reported-in-ebola-outbreak-in-dr-congo-as-six-americans-exposed-hero.jpg'
+  },
