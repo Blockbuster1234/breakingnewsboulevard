a7e442b79dd8865b08ea7469af7fd02b67f8c577
Blockbuster1234 <93834143+Blockbuster1234@users.noreply.github.com>
2026-05-18 19:06:38 +0200

    Fix: posts.js corrected

--- breakingnewsboulevard/dist/js/posts.js
+++ breakingnewsboulevard/dist/js/posts.js
@@ -7,14 +7,14 @@ const posts = [
     category: 'Test',
     url: '/posts/test-1779118698.html',
     image: '/posts/test-1779118698-hero.jpg'
-}
-];
+},
 {
     slug: 'at-least-100-deaths-reported-in-ebola-outbreak-in-dr-congo-as-six-americans-exposed',
-    title: '```json',
-    excerpt: '{   "title": "At Least 100 Deaths Reported in Ebola Outbreak in DR Congo as Six Americans Exposed",   "body": "<p>At least 100 people have died in the',
+    title: 'At Least 100 Deaths Reported in Ebola Outbreak in DR Congo',
+    excerpt: 'At least 100 people have died in the Ebola outbreak in DR Congo as six Americans were exposed.',
     date: '2026-05-18',
     category: 'World',
     url: '/posts/at-least-100-deaths-reported-in-ebola-outbreak-in-dr-congo-as-six-americans-exposed.html',
     image: '/posts/at-least-100-deaths-reported-in-ebola-outbreak-in-dr-congo-as-six-americans-exposed-hero.jpg'
-  },
+}
+];
